6a56731b5913d9689a445678ec4e62729a0d242e
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-07 19:43:18 -0300

    Adcionado input + SenEmail

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,13 +1,40 @@
-import { ContainerContact, ContainerStyled, FormContact, InputForm, TitleContact } from "./styles";
+import { ButtonForm, ContainerContact, ContainerStyled, FormContact, InputForm, SelectForm, TitleContact, TextAreaStyled } from "./styles";
+import InputMask from 'react-input-mask';
 
-export default function Contact({children}){
-    return(
-       <ContainerContact>
+export default function Contact({ children }) {
+    return (
+        <ContainerContact>
             <TitleContact>Entre em contato</TitleContact>
-            <FormContact>
-                <InputForm placeholder=" Seu nome"/>
+            <FormContact action="https://formsubmit.co/dionatannsilvaa@gmail.com" method="POST">
+                <InputForm required type="text" name="Nome" placeholder=" Seu nome" />
+                <div style={{ "display": "flex", "widows": "100%", "justifyContent": "space-between", "gap": "10px" }}>
+                    <InputMask
+                        required
+                        mask="(99) 9 9999-9999"
+                        maskChar=""
+                    >
+                        {(inputProps) => <InputForm required name="Numero" type="text" style={{ "width": "100%" }} placeholder=" (99) 9 9999-9999" {...inputProps} />}
+                    </InputMask>
+                    <InputForm required name="E-mail" style={{ "width": "100%" }} placeholder=" Seu E-mail" />
+                </div>
+                <SelectForm style={{"paddingLeft":"3px"}} name="Assunto" defaultValue="Selecione">
+                    <option value="Selecione" disabled hidden>Selecione um assunto</option>
+                    <option value="compras">Compras</option>
+                    <option value="sujestao">Sujestão</option>
+                    <option value="duvidas">Duvidas</option>
+                </SelectForm>
+                <SelectForm style={{"paddingLeft":"3px"}} name="Loja" defaultValue="Selecione">
+                    <option value="Selecione" hidden>Selecione a sua loja preferida...</option>
+                    <option value="Parada-47">Parada 47</option>
+                    <option value="Parada-52">Parada 52</option>
+                    <option value="Americanas">Americanas</option>
+                </SelectForm>
+                <TextAreaStyled required name="Mensagem" placeholder=" Mensagem"></TextAreaStyled>
+                <input type="hidden" name="_captcha" value="false"></input>
+                <input type="hidden" name="_next" value="http://localhost:3000/"></input>
+                <ButtonForm type="submit">Enviar</ButtonForm>
             </FormContact>
-       </ContainerContact>
-       
+        </ContainerContact>
+
     )
 }
--- src/components/Contact/styles.jsx
+++ src/components/Contact/styles.jsx
@@ -6,6 +6,7 @@ export const ContainerContact = styled.div`
     flex-direction: column;
     align-items: center;
     width: 920px;
+    
 `
 export const TitleContact = styled.h1`
     font-family: 'Roboto';
@@ -13,18 +14,58 @@ export const TitleContact = styled.h1`
     font-weight: 500;
     font-size: 36px;
     color: #283D50;
-    margin-top: 50px;
+    margin-top: 70px;
 `
 
 export const FormContact = styled.form`
-    margin-top: 50px;
+    margin-top: 70px;
     width: 100%;
     display: flex;
     flex-direction: column;
+    gap: 16px;
 `
 export const InputForm = styled.input`
     border: none;
     border: solid 1px #CED4DA;
     border-radius: 6px;
     height: 38px;
+    padding-left: 7px;
+    &::placeholder {
+    color: black;
+  }
+`
+export const SelectForm = styled.select`
+    width: 100%;
+    border: none;
+    border: solid 1px #CED4DA;
+    border-radius: 6px;
+    height: 38px;
+`
+
+export const ButtonForm = styled.button`
+    margin-inline: auto;
+    margin-top: 20px;
+    width: 120px;
+    height: 50px;
+    left: 900px;
+    top: 2225px;
+    background: #01649C;
+    border: none;
+    border-radius: 12px;
+    color: #CED4DA;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+`
+export const TextAreaStyled = styled.textarea`
+    height: 110px;
+    max-width: 920px;
+    resize: none;
+    border-radius: 6px;
+    border: solid 1px #CED4DA;
+    padding-left: 5px;
+    &::placeholder {
+        color: black;
+     }
 `
