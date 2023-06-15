d5283d962ae265e758839042e8d3a08113862e3a
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-15 14:17:14 -0300

    Ajuste Viewport

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html style={{scrollBehavior:'smooth'}}>
       <Head lang='pt-BR' />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
