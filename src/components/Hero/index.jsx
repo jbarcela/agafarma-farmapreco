6502e3c25b5ccdb4dfab22fb290442ae859d47c6
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-15 14:59:24 -0300

    Teste sem prioridade

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,7 +3,7 @@ import banner from "../../../public/images/banner.png"
 export default function Hero({children}){
     return(
         <ContainerHero id="hero">
-            <HeroImageStiled  src={banner} alt="hero" width={2560} height={700} priority="true"/>
+            <HeroImageStiled  src={banner} alt="hero" width={2560} height={700} priority="false"/>
             <ContainerButtons>
                 <ButtonHeroStyled><ImageButton  src="/images/icons/whatsapp-btn.png" alt="" /> Parada 47</ButtonHeroStyled>
                 <ButtonHeroStyled><ImageButton  src="/images/icons/whatsapp-btn.png" alt="" /> Parada 51</ButtonHeroStyled>
