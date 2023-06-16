8df4e4549ddb72ee91463e7c5996b73e8a461da2
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-06-15 21:04:25 -0300

    Ajuste ceo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,10 +30,13 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='pt-br' style={{scrollBehavior:'smooth'}}>
+      <Html lang='pt-BR' style={{scrollBehavior:'smooth'}}>
         <Head>
         <title>Agafarma</title>
         <meta name="description" content="Agafarma - Sua farmácia de confiança para atender às suas necessidades de saúde e bem-estar. Oferecemos uma ampla variedade de medicamentos, produtos de cuidados pessoais e serviços farmacêuticos de qualidade. Nossa equipe dedicada de profissionais farmacêuticos está pronta para fornecer orientação especializada e atendimento personalizado. Conte com a Agafarma para encontrar tudo o que você precisa para cuidar da sua saúde e da sua família. Visite-nos hoje mesmo e experimente o atendimento excepcional que nos tornou uma referência na comunidade." />
+        <meta name="keywords" content="farmácia, agafarma, remedios, medicamentos, genéricos, antibióticos, anti-alérgico, xarope, anticoncepcional,fraldas, manipulados, popular, promoção, comprimido, contato, capsula"></meta>
+        <meta name="author" content="Agafarma"/>
+        <meta name="robots" content="index,follow"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
