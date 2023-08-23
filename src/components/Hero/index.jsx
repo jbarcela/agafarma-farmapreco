685b77e76058190e7f833e8ad92ef445c9b470b3
Dionatan da Silva <dionatans@gazetadopovo.com.br>
2023-08-23 17:02:53 -0300

    Ajuste no hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,16 +1,59 @@
-import { ButtonHeroStyled, ContainerButtons, ContainerHero, HeroImageStiled,ImageButton } from "./styles";
-import styles from "../../globalCss/styles.module.css"
-import banner from "../../../public/images/banner.png"
-export default function Hero({children}){
-    return(
-        <ContainerHero >
-            <a  className={styles.anchor} id="hero"></a>
-            <HeroImageStiled  src={banner} alt="imagem do hero" width={2560} height={700} priority="false"/>
-            <ContainerButtons>
-                <ButtonHeroStyled title="click aqui para falar com nós no whatsapp parada 47" href="https://api.whatsapp.com/send?phone=5551999590358" target="_blank"><ImageButton  src="/images/icons/whatsapp-btn.png" alt="click aqui para falar com nós no whatsapp parada 47" /> Parada 47</ButtonHeroStyled>
-                <ButtonHeroStyled title="click aqui para falar com nós no whatsapp parada 51" href="https://api.whatsapp.com/send?phone=5551996387616" target="_blank"><ImageButton  src="/images/icons/whatsapp-btn.png" alt="click aqui para falar com nós no whatsapp parada 51" /> Parada 51</ButtonHeroStyled>
-                <ButtonHeroStyled title="click aqui para falar com nós no whatsapp parada Americana" href="https://api.whatsapp.com/send?phone=5551980158168" target="_blank"><ImageButton  src="/images/icons/whatsapp-btn.png" alt="click aqui para falar com nós no whatsapp parada Americana" /> Americana</ButtonHeroStyled>
-            </ContainerButtons>
-        </ContainerHero>
-    )
+import {
+  ButtonHeroStyled,
+  ContainerButtons,
+  ContainerHero,
+  HeroImageStiled,
+  ImageButton,
+} from "./styles";
+import styles from "../../globalCss/styles.module.css";
+import banner from "../../../public/images/banner-desktop.webp";
+
+export default function Hero({ children }) {
+  return (
+    <ContainerHero>
+      <a className={styles.anchor} id="hero"></a>
+      <HeroImageStiled
+        src={banner}
+        alt="imagem do hero"
+        width={banner.width}
+        height={banner.height}
+        priority="false"
+      />
+      <ContainerButtons>
+        <ButtonHeroStyled
+          title="click aqui para falar com nós no whatsapp parada 47"
+          href="https://api.whatsapp.com/send?phone=5551999590358"
+          target="_blank"
+        >
+          <ImageButton
+            src="/images/icons/whatsapp-btn.png"
+            alt="click aqui para falar com nós no whatsapp parada 47"
+          />{" "}
+          Parada 47
+        </ButtonHeroStyled>
+        <ButtonHeroStyled
+          title="click aqui para falar com nós no whatsapp parada 51"
+          href="https://api.whatsapp.com/send?phone=5551996387616"
+          target="_blank"
+        >
+          <ImageButton
+            src="/images/icons/whatsapp-btn.png"
+            alt="click aqui para falar com nós no whatsapp parada 51"
+          />{" "}
+          Parada 51
+        </ButtonHeroStyled>
+        <ButtonHeroStyled
+          title="click aqui para falar com nós no whatsapp parada Americana"
+          href="https://api.whatsapp.com/send?phone=5551980158168"
+          target="_blank"
+        >
+          <ImageButton
+            src="/images/icons/whatsapp-btn.png"
+            alt="click aqui para falar com nós no whatsapp parada Americana"
+          />{" "}
+          Americana
+        </ButtonHeroStyled>
+      </ContainerButtons>
+    </ContainerHero>
+  );
 }
--- src/components/Hero/styles.jsx
+++ src/components/Hero/styles.jsx
@@ -1,77 +1,85 @@
-import styled from "styled-components"
-import Image from "next/image"
+import styled from "styled-components";
+import Image from "next/image";
 
 export const ContainerHero = styled.div`
-    width: 100vw;
-    max-width: 100%;
-    height: auto;
-    position: relative;
-`
+  width: 100vw;
+  max-width: 100%;
+  height: auto;
+  position: relative;
+`;
 
-export const HeroImageStiled = styled(Image)`
-    width: 100%;
-    height: auto;
-`
+export const HeroImageStiled = styled(Image)``;
 export const ContainerButtons = styled.div`
-    position: absolute;
-    display: flex;
-    gap: 30px;
-    bottom: 6%;
-    right: 6%;
-    @media (max-width: 980px) {
-        gap: 18px;
-    }
-    @media (max-width: 768px) {
-        gap: 10px;
-    }
-`
+  position: absolute;
+  display: flex;
+  gap: 30px;
+  bottom: 6%;
+  right: 6%;
+  @media (max-width: 980px) {
+    gap: 18px;
+  }
+  @media (max-width: 768px) {
+    gap: 10px;
+  }
+  @media (max-width: 668px) {
+    gap: 18px;
+    width: 100%;
+    justify-content: center;
+    right: 0;
+  }
+  @media (max-width: 420px) {
+    gap: 12px;
+  }
+`;
 
 export const ButtonHeroStyled = styled.a`
-    font-family: 'Roboto';
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 12px;
-    width: 170px;
-    height: 70px;
-    background-color: #FFCE0C;
-    border: none;
-    border-radius: 10px;
-    font-size: 18px;
-    font-weight: bold;
-    color: #000;
-    &:hover{
-        opacity: 0.8;
-    }
-    @media (max-width: 1520px) {
-        width: 140px;
-        height: 60px;
-        font-size: 16px;
-    }
-    @media (max-width: 1180px) {
-        height: 50px;
-    }
-    @media (max-width: 980px) {
-        height: 40px;
-        width: 120px;
-    }
-    @media (max-width: 768px) {
-        width: 105px;
-        font-size: 14px;
-        gap: 5px;
-        height: 35px;
-    }
-    @media (max-width: 668px) {
-        display: none;
-    }
-`
+  font-family: "Roboto";
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  width: 170px;
+  height: 70px;
+  background-color: #ffce0c;
+  border: none;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #000;
+  &:hover {
+    opacity: 0.8;
+  }
+  @media (max-width: 1520px) {
+    width: 140px;
+    height: 60px;
+    font-size: 16px;
+  }
+  @media (max-width: 1180px) {
+    height: 50px;
+  }
+  @media (max-width: 980px) {
+    height: 40px;
+    width: 120px;
+  }
+  @media (max-width: 768px) {
+    width: 105px;
+    font-size: 14px;
+    gap: 5px;
+    height: 35px;
+  }
+  @media (max-width: 668px) {
+    width: 105px;
+    font-size: 14px;
+    gap: 5px;
+    height: 30px;
+  }
+`;
 export const ImageButton = styled.img`
-    width: 30px;
+  width: 30px;
+  height: auto;
+  @media (max-width: 980px) {
+    width: 20px;
     height: auto;
-    @media (max-width: 980px) {
-        width: 20px;
-        height: auto;
-    }
-    
-`
+  }
+`;
